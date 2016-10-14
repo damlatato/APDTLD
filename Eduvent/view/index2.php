28e045b700038d90dd061e62e884cacf90b80b52
maoconpa <maoconpa>
2016-10-14 20:32:00 +0200

    Added material design for bootstrap theme and already worked on a template

---
+++ Eduvent/view/index2.php
@@ -0,0 +1,15 @@
+<?php
+	include 'header.php';
+?>
+
+
+    <h1>Hello, world!</h1>
+    
+    <button type="button" class="btn btn-success">Success</button>
+    
+    <script src="includes/js/bootstrap.min.js"></script>
+  
+<?php
+	include 'footer.php';
+?>
+    
