074ca244cf90cd6cf59dadccbe14a8fdb44e1fd4
leon-l <leonlourie@yahoo.de>
2016-11-05 17:05:11 +0100

    Search tags background

--- Eduvent/controller/search-tags.php
+++ Eduvent/controller/search-tags.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default popular-search-tags">
 	<div class="panel-heading"><h5 class="font-weight-bold">Popular search tags</h5></div>
-	<div class="panel-body grey lighten-5 flex-center">
+	<div class="panel-body flex-center">
 		<ul class="tag-list">
 			<li class="tag tag-default"><a>Search tag</a></li>
 			<li class="tag tag-default"><a>Search tag</a></li>
