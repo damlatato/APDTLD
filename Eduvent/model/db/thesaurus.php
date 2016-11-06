31b36a7ae60b41a23109c75fe6fb0ef2b72198b4
leon-l <leonlourie@yahoo.de>
2016-11-06 12:43:22 +0100

    Delete copies of classes

--- Eduvent/model/db/thesaurus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$genders = ["Mr."=>"Mr.", "Mrs."=>"Mrs."];
-$interest = ["Sport"=>"Sport", "Studing"=>"Studing", "Musik"=>"Musik"];
-$eventtypes = ["Presentation"=>"Presentation", "Show"=>"Show"];
-$priceCategory = ["Expensive"=>"Expensive", "Normal"=>"Normal", "Cheap"=>"Cheap"];
-$notificationtypes = ["Message"=>"Message", "Update"=>"Update"];
-$statuses = ["Proposed"=>"Proposed", "Published"=>"Published"];
-?>
