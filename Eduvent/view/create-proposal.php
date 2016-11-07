7d13ed9cd3fb2c262b722d9bd6a67f6818ebe9e8
leon-l <leonlourie@yahoo.de>
2016-11-07 12:58:31 +0100

    Create proposal design

--- Eduvent/view/create-proposal.php
+++ Eduvent/view/create-proposal.php
@@ -19,11 +19,11 @@
 		<!--Row 2-->
 		<div class="row">
 			<div class="col-md-12">
-				<form>
-					<div class="form-group row">
-						<label for="topic-select" class="col-xs-2 col-form-label">Topic</label>
+				<form action="../Eduvent/controller/create-proposal-controller.php" method="post">
+					<div class="form-group row cp-form-group">
+						<label for="cp-topic" class="col-xs-2 col-form-label">Topic</label>
 						<div class="col-xs-3">
-							<select class="form-control" id="topic-select">
+							<select class="form-control" id="cp-topic" name="cp-topic">
 								<option>Topic 1</option>
 								<option>Topic 2</option>
 								<option>Topic 3</option>
@@ -31,51 +31,53 @@
 						</div>
 					</div>
 
-					<div class="form-group row">
-						<label for="title-input" class="col-xs-2 col-form-label">Title</label>
+					<div class="form-group row cp-form-group">
+						<label for="cp-title" class="col-xs-2 col-form-label">Title</label>
 						<div class="col-xs-3">
-							<input class="form-control" type="search" value="Title" id="title-input">
+							<input class="form-control" type="search" id="cp-title" name="cp-title" placeholder="Title">
 						</div>
 					</div>
 
-					<div class="form-group row">
-						<label for="description-textarea" class="col-xs-2 col-form-label">Description</label>
-						<div class="col-xs-6">
-							<i class="fa fa-pencil prefix"></i>
-							<textarea type="text" id="description-textarea" class="form-control md-textarea" placeholder="Description"></textarea>
+					<div class="form-group row cp-form-group">
+						<label for="cp-description" class="col-xs-2 col-form-label">
+							Description &nbsp;
+							<i class="fa fa-pencil prefix" style="font-size:1.45em;"></i>
+						</label>
+						<div class="col-xs-6 md-form">
+							
+							<textarea type="text" class="form-control md-textarea" id="cp-description" name="cp-description" placeholder="Description"></textarea>
 						</div>
 					</div>
 
-					<div class="form-group row">
+					<div class="form-group row cp-form-group">
 						<p class="col-xs-3 col-form-label">Acceptable time period</p>
 					</div>
 
-					<div class="form-group row">
-						<label for="end-date" class="col-xs-2 col-form-label">From</label>
-						<div class="col-xs-3">
-							<input class="form-control" type="date" value="2016-10-20" id="start-date">
+					<div class="form-group row cp-form-group">
+						<div class="col-xs-2"></div>
+						<label for="cp-from" class="col-xs-1 col-form-label">From</label><!--text-xs-right-->
+						<div class="col-xs-2">
+							<input type="date" class="form-control" id="cp-from" name="cp-from" placeholder="yyyy-mm-dd">
+						</div>
+
+						<label for="cp-to" class="col-xs-1 col-form-label">To</label>
+						<div class="col-xs-2">
+							<input type="date" class="form-control" id="cp-to" name="cp-to" placeholder="yyyy-mm-dd">
 						</div>
 					</div>
 
-					<div class="form-group row">
-						<label for="end-date" class="col-xs-2 col-form-label">To</label>
-						<div class="col-xs-3">
-							<input class="form-control" type="date" value="2016-12-20" id="end-date">
-						</div>
-					</div>
-
-					<div class="form-group row">
-						<label for="location-input" class="col-xs-2 col-form-label">Acceptable location</label>
-						<div class="col-xs-3">
-							<input class="form-control" type="search" value="Acceptable location" id="location-input">
+					<div class="form-group row cp-form-group">
+						<label for="cp-location" class="col-xs-2 col-form-label">Acceptable location</label>
+						<div class="col-xs-6">
+							<input type="search" class="form-control" id="cp-location" name="cp-location" placeholder="Acceptable location">
 						</div>
 					</div>
 
 					<hr>
 
-					<div class="form-group row">
+					<div class="form-group row cp-form-group">
 						<div class="col-xs-8 flex-center">
-							<a href="#" class="btn btn-post-proposal pull-right">Post proposal</a>
+							<button type="submit" class="btn btn-post-proposal pull-right">Post proposal</button>
 						</div>
 					</div>
 				</form>
