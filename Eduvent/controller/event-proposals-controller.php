7b1f77b8a5b9a988ba51f10cf5c40f8e9832da48
leon-l <leonlourie@yahoo.de>
2016-11-08 01:47:53 +0100

    proposals

--- Eduvent/controller/event-proposals-controller.php
+++ Eduvent/controller/event-proposals-controller.php
@@ -10,6 +10,7 @@ foreach($proposedEvents as $event) {
 			<a href="#!" class="proposal-title"><h4>' . $event->getTitle() . '</h4></a>
 			<p>' . $event->getDescription() . '</p>
 			<div class="read-more text-xs-right">
+				<small class="pull-left"><i>Preferred location: ' . $event->getLocation()->getcity() . '</i></small>
 				<span class="requested-by">Voted by <b>' . '35' . '</b> users</span>
 				<a href="#!" class="btn btn-vote">I want it also! Vote here!</a>
 				<a href="#!" class="btn btn-offer">Offer event</a>
