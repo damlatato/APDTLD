8074a532e4f82dcf635f837f1b470af78417e6fe
leon-l <leonlourie@yahoo.de>
2016-11-08 11:10:16 +0100

    proposals

--- Eduvent/controller/create-proposal-controller.php
+++ Eduvent/controller/create-proposal-controller.php
@@ -5,10 +5,10 @@ $cp_description=$_POST["cp-description"];
 $cp_city       =$_POST["cp-city"];
 
 $address=new Address(null, null, null, $cp_city, null, null);
-$event=new Event(99, null, $cp_title, $cp_description, null, $address, $cp_topic, null, null, null);
+$event=new Event(uniqid(), null, $cp_title, $cp_description, null, $address, $cp_topic, null, null, null); //alternative:md5($str)
 $event->deleteEvent();
 
-$user=new User(9999,"Leon Lourie","leonlourie@yahoo.de","213322", $address, "m", "18.01.1990", null, null);
+$user=new User(uniqid(),"Leon Lourie","leonlourie@yahoo.de","213322", $address, "m", "18.01.1990", null, null);
 $user->deleteUser();
 $user->postUser();
 
