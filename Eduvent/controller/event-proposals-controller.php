bdc3925dbbc402b5a99bd4c6f7f966185d5da961
leon-l <leonlourie@yahoo.de>
2016-11-09 23:44:57 +0100

    vote

--- Eduvent/controller/event-proposals-controller.php
+++ Eduvent/controller/event-proposals-controller.php
@@ -42,7 +42,7 @@ foreach($proposedEvents as $event) {
 									We will inform you, if a company offers this event.</p>
 								</div>
 								<div class="modal-footer text-xs-center">
-									<button type="button" class="btn btn-standard" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-standard" data-dismiss="modal" onclick="window.location.reload()">Close</button>
 								</div>
 							</div>
 						</div>
