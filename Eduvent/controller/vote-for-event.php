c4e2daff4f2cd96e92f9f05bf74484695735ca48
leon-l <leonlourie@yahoo.de>
2016-11-09 22:55:27 +0100

    ...

--- Eduvent/controller/vote-for-event.php
+++ Eduvent/controller/vote-for-event.php
@@ -1,4 +1,11 @@
 <?php
+/*function __autoload($class)
+{
+  include_once '../Eduvent/model/'.$class.'.php';
+}*/
+
+include_once ROOT_DIR.'/model/User.php';
+
 $user=new User(uniqid(),"Leon Lourie","leonlourie@yahoo.de","12345", $address, "m", "18.01.1990", null, null);
 $user->deleteUser();
 $user->postUser();
@@ -6,6 +13,6 @@ $user->postUser();
 $event=Event::getById($_POST['proposalId']);
 $user->voteEvent($event);
 
-$output="vote-id=" . $_POST['proposalId'];
+$output="vote-id=" . $_POST['proposalId'] . " / " . $event->getId();
 echo $output;
 ?>
