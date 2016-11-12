8ddfca9ad3252364fd347fcfd94bff33d325dc24
leon-l <leonlourie@yahoo.de>
2016-11-12 17:03:08 +0100

    search bar

--- Eduvent/view/proposal-filter-bar.php
+++ Eduvent/view/proposal-filter-bar.php
@@ -1,6 +1,6 @@
 <script>
 $(document).ready(function() {
-	$('.proposal-filter-menu > li').click(function() {
+	$('.custom-dropdown-menu > li').click(function() {
 		$('#btn-proposal-filter').text($(this).text());
 		$(this).prop('hidden', true);
 		$(this).siblings().prop('hidden', false);
@@ -8,47 +8,35 @@ $(document).ready(function() {
 });
 </script>
 
+<h5 class="font-weight-bold">Event search</h5>
 <nav class="navbar navbar-light grey lighten-5">
-	<div class="collapse navbar-toggleable-xs">
-		<div class="pull-xs-left">
-
-			<form class="form-inline" id="search-form" onsubmit="runSearch()">
-				<i class="fa fa-search" aria-hidden="true"></i><input class="form-control" id="search-input" type="text" placeholder="Search proposals">
-				<span>within
-
-					<div class="custom-dropdown-btn">
-						<button class="btn btn-info-outline waves-effect dropdown-toggle" id="btn-set-radius" type="button">5 km</button>
-						<ul class="custom-dropdown-menu">
-							<li class="dropdown-item" hidden>5 km</li>
-							<li class="dropdown-item">10 km</li>
-							<li class="dropdown-item">25 km</li>
-							<li class="dropdown-item">50 km</li>
-							<li class="dropdown-item">No limit</li>
-						</ul>
-					</div>
-
-					from
-					<div class="location-search">
-						<button class="btn-location-search btn btn-info-outline waves-effect dropdown-toggle" type="button">Mannheim, DE</button>
-						<div class="location-search-menu">
-							<input type="text" class="form-control" placeholder="Location ..." aria-describedby="location-addon1">
-						</div>
-					</div>
-
-					<div class="proposal-filter">
-						Sort by
-						<button class="btn btn-info-outline waves-effect dropdown-toggle" id="btn-proposal-filter" type="button">Topic</button>
-						<ul class="proposal-filter-menu">
-							<li class="dropdown-item" hidden>Topic</li>
-							<li class="dropdown-item">Location</li>
-							<li class="dropdown-item">Newest</li>
-							<li class="dropdown-item">Most voted</li>
-						</ul>
-					</div>
-
-				</span>
-			</form>
-
+	<input class="search-input" id="search-input" type="search-md" placeholder="Search" style="width:20%;">
+	within
+	<div class="custom-dropdown-btn">
+		<div class="btn btn-info-outline waves-effect" id="btn-set-radius">5 km</div>
+		<ul class="custom-dropdown-menu">
+			<li hidden>5 km</li>
+			<li>10 km</li>
+			<li>25 km</li>
+			<li>50 km</li>
+			<li>No limit</li>
+		</ul>
+	</div>
+	from
+	<div class="custom-dropdown-btn">
+		<button class="btn-location-search btn btn-info-outline waves-effect" type="button">Mannheim, DE</button>
+		<div class="custom-dropdown-menu">
+			<input type="text" class="form-control" placeholder="Location ..." aria-describedby="location-addon1">
 		</div>
+	</div>
+	<div class="custom-dropdown-btn">
+		Sort by
+		<button class="btn btn-info-outline waves-effect" id="btn-proposal-filter" type="button">Topic</button>
+		<ul class="custom-dropdown-menu">
+			<li hidden>Topic</li>
+			<li>Location</li>
+			<li>Newest</li>
+			<li>Most voted</li>
+		</ul>
 	</div>
 </nav>
--- Eduvent/view/search-bar.php
+++ Eduvent/view/search-bar.php
@@ -10,25 +10,23 @@ $(document).ready(function() {
 
 <h5 class="font-weight-bold">Event search</h5>
 <nav class="navbar navbar-light grey lighten-5">
-		<div id="search-form">
-			<input class="search-input" id="search-input" type="search-md" placeholder="Search" style="width:20%;">
-			within
-			<div class="custom-dropdown-btn">
-				<div class="btn btn-info-outline waves-effect" id="btn-set-radius">5 km</div>
-				<ul class="custom-dropdown-menu">
-					<li hidden>5 km</li>
-					<li>10 km</li>
-					<li>25 km</li>
-					<li>50 km</li>
-					<li>No limit</li>
-				</ul>
-			</div>
-			from
-			<div class="custom-dropdown-btn">
-				<button class="btn-location-search btn btn-info-outline waves-effect dropdown-toggle" type="button">Mannheim, DE</button>
-				<div class="custom-dropdown-menu">
-					<input type="text" class="form-control" placeholder="Location ..." aria-describedby="location-addon1">
-				</div>
-			</div>
+	<input class="search-input" id="search-input" type="search-md" placeholder="Search" style="width:20%;">
+	within
+	<div class="custom-dropdown-btn">
+		<div class="btn btn-info-outline waves-effect" id="btn-set-radius">5 km</div>
+		<ul class="custom-dropdown-menu">
+			<li hidden>5 km</li>
+			<li>10 km</li>
+			<li>25 km</li>
+			<li>50 km</li>
+			<li>No limit</li>
+		</ul>
+	</div>
+	from
+	<div class="custom-dropdown-btn">
+		<button class="btn-location-search btn btn-info-outline waves-effect" type="button">Mannheim, DE</button>
+		<div class="custom-dropdown-menu">
+			<input type="text" class="form-control" placeholder="Location ..." aria-describedby="location-addon1">
 		</div>
+	</div>
 </nav>
