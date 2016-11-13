47200241a9c55fee63c448e9f96cf5cf42634b8c
leon-l <leonlourie@yahoo.de>
2016-11-13 15:09:08 +0100

    thesaurus

--- Eduvent/model/thesaurus.php
+++ Eduvent/model/thesaurus.php
@@ -1,7 +1,12 @@
 <?php
 $genders = ["Mr."=>"Mr.", "Mrs."=>"Mrs."];
-$interest = ["Sport"=>"Sport", "Studing"=>"Studing", "Musik"=>"Musik"];
-$eventtypes = ["Presentation"=>"Presentation", "Show"=>"Show"];
+$topics = ["Art"=>"Art", "Business"=>"Business", "Cooking"=>"Cooking", "Design"=>"Design",
+"Entrepreneurship"=>"Entrepreneurship", "Finance"=>"Finance",
+"Health"=>"Health", "HR"=>"HR", "Science"=>"Science", "Sports"=>"Sports", "Technology"=>"Technology"]
+$interest = $topics;
+$eventtypes = ["Conference"=>"Conference", "Course"=>"Course", "Forum"=>"Forum", "Hackathon"=>"Hackathon", "Meeting"=>"Meeting",
+				"Online-course"=>"Online-course", "Meeting"=>"Meeting", "Presentation"=>"Presentation", "Seminar"=>"Seminar",
+				"Tutorial"=>"Tutorial", "Workshop"=>"Workshop"]
 $priceCategory = ["Expensive"=>"Expensive", "Normal"=>"Normal", "Cheap"=>"Cheap"];
 $notificationtypes = ["Message"=>"Message", "Update"=>"Update"];
 $statuses = ["Proposed"=>"Proposed", "Published"=>"Published"];
