7f0d099ef180db19bdf90c211147d449db32fe5c
leon-l <leonlourie@yahoo.de>
2016-11-13 15:56:50 +0100

    sidebar (event market)

--- Eduvent/view/sidebar.php
+++ Eduvent/view/sidebar.php
@@ -1,15 +1,9 @@
 <script>
-function getEventTypes() {
-	$.post( "../Eduvent/controller/sidebar-controller.php", { x: x })
-	.done(function( data ) {
-		console.log( "vote submitted: " + data );
-	});
-}
-
 $( document ).ready(function() {
 	$('#clear-date-from').click(function(){
 		$('#datepicker-from-sidebar').val('');
 	});
+
 	$('#clear-date-to').click(function(){
 		$('#datepicker-to-sidebar').val('');
 	});
@@ -22,54 +16,11 @@ $( document ).ready(function() {
 	<div class="list-group-item sidebar-head white-text">
 		<span class="font-weight-bold">Event filters</span>
 	</div>
-	<!--Group 'Event types'-->
-	<div class="list-group-item sidebar-group-head" data-toggle="collapse" href="#collapseTypes" aria-expanded="false" aria-controls="collapseTypes">
-		<a>Event types<span class="pull-right"><i class="fa fa-caret-down" aria-hidden="true"></i></span></a>
-	</div>
-	<div class="collapse" id="collapseTypes"><!--add class 'in' to expand-->
-		<div>
-			<ul class="list-group">
-				<?php
-					include("../Eduvent/controller/sidebar-controller.php");
-				?>
-			</ul>
-		</div>
-	</div>
-	<!--Group 'Topics'-->
-	<div class="list-group-item sidebar-group-head" data-toggle="collapse" href="#collapseCategories" aria-expanded="false" aria-controls="collapseCategories">
-		<a>Topics<span class="pull-right"><i class="fa fa-caret-down" aria-hidden="true"></i></span></a>
-	</div>
-	<div class="collapse" id="collapseCategories"><!--add class 'in' to expand-->
-		<div>
-			<ul class="list-group">
-				<li class="list-group-item"><a href="#">Art</a></li>
-				<li class="list-group-item"><a href="#">Business</a></li>
-				<li class="list-group-item"><a href="#">Cooking</a></li>
-				<li class="list-group-item"><a href="#">Design</a></li>
-				<li class="list-group-item"><a href="#">Entrepreneurship</a></li>
-				<li class="list-group-item"><a href="#">Finance</a></li>
-				<li class="list-group-item"><a href="#">Health</a></li>
-				<li class="list-group-item"><a href="#">HR</a></li>
-				<li class="list-group-item"><a href="#">Science</a></li>
-				<li class="list-group-item"><a href="#">Sports</a></li>
-				<li class="list-group-item"><a href="#">Technology</a></li>
-				<li class="list-group-item font-weight-bold"><a href="#"><b><i>More ...</i></b></a></li>
-			</ul>
-		</div>
-	</div>
-	<!--Group 'Pricing'-->
-	<div class="list-group-item sidebar-group-head" data-toggle="collapse" href="#collapsePricing" aria-expanded="false" aria-controls="collapsePricing">
-		<a>Pricing<span class="pull-right"><i class="fa fa-caret-down" aria-hidden="true"></i></span></a>
-	</div>
-	<div class="collapse" id="collapsePricing"><!--add class 'in' to expand-->
-		<div>
-			<ul class="list-group">
-				<li class="list-group-item"><a href="#">All</a></li>
-				<li class="list-group-item"><a href="#">Free</a></li>
-				<li class="list-group-item"><a href="#">Paid</a></li>
-			</ul>
-		</div>
-	</div>
+
+	<?php
+		include ("../Eduvent/controller/sidebar-controller.php");
+	?>
+
 	<!--Group 'Date'-->
 	<div class="list-group-item sidebar-group-head" data-toggle="collapse" href="#collapseDate" aria-expanded="false" aria-controls="collapseDate">
 		<a>Date<span class="pull-right"><i class="fa fa-caret-down" aria-hidden="true"></i></span></a>
