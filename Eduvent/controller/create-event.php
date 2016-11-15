235ee42147bd3220008b793611c21b568a40e1b1
lgunko <leonidgunko1@yandex.ru>
2016-11-15 20:04:46 +0100

    Add files via upload

---
+++ Eduvent/controller/create-event.php
@@ -0,0 +1,95 @@
+<div class="msf-container">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 msf-title">
+				<h1>Create Your Event</h1>
+				<h3><small>Please complete the form below to create and publish your event.</small></h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 msf-form">
+				
+				<form role="form" action="" method="post" class="form-inline">
+					
+					<fieldset>
+						<h4>Introduction <span class="step">(Step 1 / 3)</span></h4>
+						<div class="form-group">
+							<label for="first-name">First Name:</label><br>
+							<input type="text" name="first-name" class="first-name form-control" id="first-name">
+						</div>
+						<div class="form-group">
+							<label for="last-name">Last Name:</label><br>
+							<input type="text" name="last-name" class="last-name form-control" id="last-name">
+						</div>
+						<br>
+						<button type="button" class="btn btn-next">Next <i class="fa fa-angle-right"></i></button>
+					</fieldset>
+					
+					<fieldset>
+						<h4>Place and Date of Birth <span class="step">(Step 2 / 3)</span></h4>
+						<div class="form-group">
+							<label for="birth-city">City:</label><br>
+							<input type="text" name="birth-city" class="birth-city form-control" id="birth-city">
+						</div>
+						<div class="form-group">
+							<label for="birth-state">State / Province:</label><br>
+							<input type="text" name="birth-state" class="birth-state form-control" id="birth-state">
+						</div>
+						<div class="form-group">
+							<label for="birth-country">Country:</label><br>
+							<input type="text" name="birth-country" class="birth-country form-control" id="birth-country">
+						</div>
+						<div class="form-group">
+							<label for="birth-date">Date (YYYY/MM/DD):</label><br>
+							<input type="text" name="birth-date" class="birth-date form-control" id="birth-date">
+						</div>
+						<br>
+						<button type="button" class="btn btn-previous"><i class="fa fa-angle-left"></i> Previous</button>
+						<button type="button" class="btn btn-next">Next <i class="fa fa-angle-right"></i></button>
+					</fieldset>
+					
+					<fieldset>
+						<h4>Address and Contact Information <span class="step">(Step 3 / 3)</span></h4>
+						<div class="form-group">
+							<label for="address">Address:</label><br>
+							<input type="text" name="address" class="address form-control" id="address">
+						</div>
+						<div class="form-group">
+							<label for="address-city">City:</label><br>
+							<input type="text" name="address-city" class="address-city form-control" id="address-city">
+						</div>
+						<div class="form-group">
+							<label for="address-state">State / Province:</label><br>
+							<input type="text" name="address-state" class="address-state form-control" id="address-state">
+						</div>
+						<div class="form-group">
+							<label for="address-country">Country:</label><br>
+							<input type="text" name="address-country" class="address-country form-control" id="address-country">
+						</div>
+						<div class="form-group">
+							<label for="address-postal-code">Postal Code:</label><br>
+							<input type="text" name="address-postal-code" class="address-postal-code form-control" id="address-postal-code">
+						</div>
+						<div class="form-group">
+							<label for="telephone">Telephone:</label><br>
+							<input type="text" name="telephone" class="telephone form-control" id="telephone">
+						</div>
+						<div class="form-group">
+							<label for="mobile-phone">Mobile Phone:</label><br>
+							<input type="text" name="mobile-phone" class="mobile-phone form-control" id="mobile-phone">
+						</div>
+						<div class="form-group">
+							<label for="email">Email:</label><br>
+							<input type="text" name="email" class="email form-control" id="email">
+						</div>
+						<br>
+						<button type="button" class="btn btn-previous"><i class="fa fa-angle-left"></i> Previous</button>
+						<button type="button" class="btn btn-next">Next <i class="fa fa-angle-right"></i></button>
+					</fieldset>
+
+				</form>
+				
+			</div>
+		</div>
+	</div>
+</div>
