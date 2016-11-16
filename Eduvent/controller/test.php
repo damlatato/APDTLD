2c24c0027839b4e743a485deb6bdd4779087634c
Leon <leonlourie@yahoo.de>
2016-11-17 00:03:16 +0100

    filterEvents()

--- Eduvent/controller/test.php
+++ Eduvent/controller/test.php
@@ -1,3 +1,43 @@
 <?php
 echo 's: ' . $_SERVER["DOCUMENT_ROOT"];
+
+public static function filterEvents($status, $type, $topic, $priceCategory, $StartDate, $EndDate){
+
+	if ($status=='') {
+		$status="Published";
+	}
+	$urlStatus="status:".chr(34).$status.chr(34);
+
+	$urlType='';
+	if ($type!=='') {
+		$urlType="+type:".chr(34).$type.chr(34);
+	}
+
+	$urlTopic='';
+	if ($topic!=='') {
+		$urlTopic="+topic:".chr(34).$topic.chr(34);
+	}
+
+	$urlPriceCat='';
+	if ($priceCategory!=='') {
+		$urlPriceCat="+topic:".chr(34).$priceCategory.chr(34);
+	}
+	
+	$url = "event?pageNumber=1&pageSize=500&q=" . $urlStatus . $urlType . $urlTopic . $urlPriceCat;
+
+	$jeventlist = get($url);
+	$eventlist = Event::fromJSONa($jeventlist);
+	$eventlist2 = array();
+	
+	if ($StartDate!=='' or $EndDate!=='') {
+		foreach($eventlist as $event){
+			if(strtotime($event->getDatetime())>strtotime($StartDate) && strtotime($event->getDatetime())<strtotime($EndDate)){
+				array_push($eventlist2, $event);
+			}
+		}
+		$eventlist=$eventlist2;
+	}
+	
+	return $eventlist;
+}
 ?>
--- Eduvent/model/Event.php
+++ Eduvent/model/Event.php
@@ -290,6 +290,45 @@ class Event  implements JsonSerializable{
 		$jeventlist = get("event?pageNumber=1&pageSize=500&q=priceCategory:".chr(34).$priceCategory.chr(34));
 		return Event::fromJSONa($jeventlist);
 	}
+
+	public static function filterEvents($status, $type, $topic, $priceCategory, $StartDate, $EndDate){
+		if ($status=='') {
+			$status="Published";
+		}
+		$urlStatus="status:".chr(34).$status.chr(34);
+
+		$urlType='';
+		if ($type!=='') {
+			$urlType="+type:".chr(34).$type.chr(34);
+		}
+
+		$urlTopic='';
+		if ($topic!=='') {
+			$urlTopic="+topic:".chr(34).$topic.chr(34);
+		}
+
+		$urlPriceCat='';
+		if ($priceCategory!=='') {
+			$urlPriceCat="+topic:".chr(34).$priceCategory.chr(34);
+		}
+		
+		$url = "event?pageNumber=1&pageSize=500&q=" . $urlStatus . $urlType . $urlTopic . $urlPriceCat;
+
+		$jeventlist = get($url);
+		$eventlist = Event::fromJSONa($jeventlist);
+		$eventlist2 = array();
+		
+		if ($StartDate!=='' or $EndDate!=='') {
+			foreach($eventlist as $event){
+				if(strtotime($event->getDatetime())>strtotime($StartDate) && strtotime($event->getDatetime())<strtotime($EndDate)){
+					array_push($eventlist2, $event);
+				}
+			}
+			$eventlist=$eventlist2;
+		}
+		
+		return $eventlist;
+	}
 	
 	public function addOffer($organizedEvent){	//$this - Event Proposal
 		if ($this->getStatus()=="Proposed"){
