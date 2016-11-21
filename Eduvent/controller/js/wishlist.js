3853ad3f43981f3933d065cb5d472ca6c5fe2f8d
lgunko <leonidgunko1@yandex.ru>
2016-11-21 20:31:05 +0100

    Add files via upload

---
+++ Eduvent/controller/js/wishlist.js
@@ -0,0 +1,21 @@
+$('.add-to-wishlist').click(function() {
+	$eventID = $(this).attr("eventid");
+	$userMail = $(this).attr("usermail");
+	alert("in wishlist js"+$eventID + '   '+ $userMail);
+	$.ajax({
+	  type: "POST",
+	  url: "controller/wishlistHandler.php",
+	  data: {
+		functionname: 'addToWishlist',
+		eventID: $eventID,
+		userMail: $userMail
+	  },
+	  dataType: "text"
+	}).done(function( msg ) {
+		alert( "done: " + msg );
+	}).fail(function( msg ) {
+	  alert( "Request failed: " + msg );
+	});
+	
+
+});
