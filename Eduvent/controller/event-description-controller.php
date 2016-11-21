76338780ecc6e0dfd1f24cf0b5d18f38a1e16dcb
lgunko <leonidgunko1@yandex.ru>
2016-11-21 20:35:11 +0100

    Add files via upload

--- Eduvent/controller/event-description-controller.php
+++ Eduvent/controller/event-description-controller.php
@@ -6,13 +6,6 @@
 	
 
 
-<div class="alert alert-success" role="alert" id="successfulbuyed" style="display:none;">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		<span aria-hidden="true">&times;</span>
-	</button>
-	<strong>Event added to shopping cart!</strong> Go to <a href="#" class="alert-link">Shopping cart </a>to see the content.
-</div>
-
 <div id="content">
     <div class="col-md-12">
 		<div class="row">
--- Eduvent/controller/event-market-controller.php
+++ Eduvent/controller/event-market-controller.php
@@ -1,12 +1,6 @@
 <script type="text/javascript" src="../Eduvent/controller/js/shoppingCart.js"></script>
 <script type="text/javascript" src="../Eduvent/controller/js/wishlist.js"></script>
 
-<div class="alert alert-success" role="alert" id="successfulbuyed" style="display:none;">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		<span aria-hidden="true">&times;</span>
-	</button>
-	<strong>Event added to shopping cart!</strong> Go to <a href="#" class="alert-link">Shopping cart </a>to see the content.
-</div>
 
 <?php
 if (isset($_POST['root-path'])) {
