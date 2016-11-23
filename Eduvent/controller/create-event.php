6f0f66ab9a14d0a3afa3bd366afb8d62dd8b18cb
maoconpa <maoconpa>
2016-11-23 16:41:20 +0100

    ...

--- Eduvent/controller/create-event.php
+++ Eduvent/controller/create-event.php
@@ -60,12 +60,14 @@ if($check === true){
 	}
 	// Check if $uploadOk is set to 0 by an error
 	if ($uploadOk == 0) {
+		$target_file_event = '../Eduvent/view/images/event-img.png';
 		echo "Sorry, your file was not uploaded.";
 		// if everything is ok, try to upload file
 	} else {
 		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		} else {
+			$target_file_event = '../Eduvent/view/images/event-img.png';
 			echo "Sorry, there was an error uploading your file.";
 		}
 	}
