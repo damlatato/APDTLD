526777163ce8c0b515e596d67f003ff81c34b279
maoconpa <maoconpa>
2016-11-24 18:36:11 +0100

    ..

--- Eduvent/view/search-tags.php
+++ Eduvent/view/search-tags.php
@@ -2,13 +2,11 @@
 	<div class="panel-heading"><h5 class="font-weight-bold">Popular search tags</h5></div>
 	<div class="panel-body flex-center">
 		<ul class="tag-list">
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
-			<li class="tag tag-default"><a>Search tag</a></li>
+			<li class="tag tag-default"><a>Advanced Informatics</a></li>
+			<li class="tag tag-default"><a>Technology presentation</a></li>
+			<li class="tag tag-default"><a>Scrum Process</a></li>
+			<li class="tag tag-default"><a>Design Thinking</a></li>
+			<li class="tag tag-default"><a>Play Piano</a></li>
 		</ul>
 	</div>
 </div>
