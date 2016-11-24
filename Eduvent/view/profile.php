12a769e65dee04647e5b4561417d2e97e728f25e
maoconpa <maoconpa>
2016-11-24 01:28:58 +0100

    ...

--- Eduvent/view/profile.php
+++ Eduvent/view/profile.php
@@ -1,13 +1,74 @@
 <?php
 include("../Eduvent/controller/profile-controller.php");
 
+if(isset($_POST['submitProfile'])) {
+	
+	$target_event_dir = "../Eduvent/view/images/eventimages/";
+	$target_dir = "../Eduvent/view/images/eventimages/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$target_file_event = $target_event_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}
+	}
+	
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		$target_file_event = '../Eduvent/view/images/event-img.png';
+		echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		} else {
+			$target_file_event = '../Eduvent/view/images/event-img.png';
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}
+	
+	User::getUserByEmail($userEmail)->setimgHref($target_file_event);
+	$userImg = $target_file_event;
+}
 ?>
+
 
 <div class="container">
 	<div class="row">
 		<div class="col-md-12 text-xs-center">
-			<div><img src=<?php echo $userImg; ?> class="img-responsive" height="140">
-			<a class="btn btn-default"><i class="fa fa-magic left"></i> Edit image</a></div>
+			<div><img src=<?php echo $userImg; ?> class="img-responsive" height="140"></div>
+
+			<form action="" method="post" enctype="multipart/form-data">
+   					 Select image to upload:
+   			 	<input type="file" name="fileToUpload" id="fileToUpload">
+   			 	<button type="submit" class="btn" name="submitProfile" style="background: #1694b2;">Upload</button>
+    			
+			</form>
+			
 			<h1><div class="editable-name"></div></h1>
 		</div>
 	</div><br>
